cf0954f36334c0e1ebcc050d014d95e7d671d72d
Frank Podraza <frankpodraza@icloud.com>
2026-09-12 15:58:49 -0700

    made audio manager

--- autoloads/audio_manager.gd
+++ autoloads/audio_manager.gd
@@ -1,10 +1,25 @@
 extends Node3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var sound_effects_bus_name: String = "SoundEffects"
 
+func play_sound_effect(sound: AudioStream, positional: bool = false) -> void:
+	var pitch_range: Vector2 = Vector2(0.95, 1.05)
+	var volume_db: float = 1.0
+	var pause_behaviour = PROCESS_MODE_INHERIT
+	
+	if sound != null:
+		var stream_player = AudioStreamPlayer.new() if !positional else AudioStreamPlayer3D.new()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		stream_player.stream = sound
+		stream_player.bus = sound_effects_bus_name
+		stream_player.finished.connect(stream_player.queue_free)
+		stream_player.pitch_scale = randf_range(pitch_range.x, pitch_range.y)
+		stream_player.volume_db = volume_db
+
+		# Add the new stream player to scene tree
+		add_child(stream_player)
+		if positional:
+			print("set position, attenuation filter cutoff, and volume, of the sound")
+
+		# Play the sound
+		stream_player.play()
